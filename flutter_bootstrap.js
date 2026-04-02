9fd8b3c5387fab4641c8deadebe8702454f4f64d
Efe Şahin <efesahin@Efe-Mac-mini.local>
2026-04-02 11:53:19 +0200

    deploy: update web app with tappable plan cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2223836863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4082786425" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
