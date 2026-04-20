4df25723303d8d4ac6968a2ae9def807df34cb85
Efe Sahin <efesahin38@gmail.com>
2026-04-20 16:34:14 +0200

    GWS Tagesplan PDF & Photo Fix v19.6.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1825105510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "720522823" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
